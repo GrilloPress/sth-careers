87a0f998d4ccc5f4ee8ed4e469841ed1c27d0994
GrilloPress <grillopress@gmail.com>
2015-07-17 20:48:56 +0000

    add logo

--- wordpress/wp-content/themes/wp-careers/header.php
+++ wordpress/wp-content/themes/wp-careers/header.php
@@ -38,12 +38,12 @@
 <header role="banner">
   
   
-      <div id="logos">
+      <div id="site-header">
         <div class="container">
           <div class="row">
             <div class="col-md-12">
               <a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
-              
+              <img class="pull-right" src="<?php echo get_template_directory_uri() . "/images/logo.gif"; ?>">
             </div>
           </div>
         </div>

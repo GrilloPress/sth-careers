a4064420e139b2391b0e7ccdf12719ebd52970fc
GrilloPress <grillopress@gmail.com>
2015-07-17 19:21:16 +0000

    removed duplicate theme folder

--- wp-careers/sidebar.php
+++
@@ -1,9 +0,0 @@
-				<div id="sidebar1" class="col-sm-3 col-xs-3" role="complementary">
-				
-					<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
-
-						<?php dynamic_sidebar( 'sidebar1' ); ?>
-
-					<?php endif; ?>
-
-				</div>

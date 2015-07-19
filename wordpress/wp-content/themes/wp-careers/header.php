638e502700c076fecaba20e11e19d1b1bd035625
GrilloPress <grillopress@gmail.com>
2015-07-19 13:43:41 +0000

    update sass project structure

--- wordpress/wp-content/themes/wp-careers/header.php
+++ wordpress/wp-content/themes/wp-careers/header.php
@@ -36,9 +36,7 @@
 	<body <?php body_class(); ?>>
 				
 <header role="banner">
-  
-  
-      <div id="site-header">
+      <div id="site-header" class="hidden-xs hidden-sm">
         <div class="container">
           <div class="row">
             <div class="col-md-12">
@@ -59,20 +57,10 @@
 							<span class="icon-bar"></span>
               <span class="icon-bar"></span>
 						</button>
-
-						<a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
 					</div>
 
 					<div class="collapse navbar-collapse navbar-responsive-collapse">
 						<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
-
-						<?php //if(of_get_option('search_bar', '1')) {?>
-						<form class="navbar-form navbar-right" role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-							<div class="form-group">
-								<input name="s" id="s" type="text" class="search-query form-control" autocomplete="off" placeholder="<?php _e('Search','wpbootstrap'); ?>">
-							</div>
-						</form>
-						<?php //} ?>
 					</div>
 
 				</div> <!-- end .container -->

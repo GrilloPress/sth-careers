5436c70b44a15a50c30b9d0dd927e4b1767c996c
GrilloPress <grillopress@gmail.com>
2015-08-18 11:23:12 +0000

    update andrew cash intro card

--- wordpress/wp-content/themes/sth/page-home.php
+++ wordpress/wp-content/themes/sth/page-home.php
@@ -14,34 +14,40 @@
       <main id="main" role="main">
         <?php get_template_part( 'partials/content', 'slider' ); ?>
         
-        <div class="home-intro">
-             <div class="row">
-              <div class="col-md-5">
-                <h2 class="well-heading">Join the Team!</h2>
-                <div class="well">
-                  <p>The affix plugin toggles between three classes, each representing a particular state: .affix, .affix-top, and .affix-bottom. You must provide the styles, with the exception of position: fixed; on .affix, for these classes yourself (independent of this plugin) to handle the actual positions.</p>
-                  <p><strong>Sir Andrew Cash, OBE</strong></p>
-                  <div class="text-center">
-                     <a href="<?php echo get_home_url() . "/current-vacancies/" ;?>" class="btn btn-primary">Current Vacancies</a>
+        <section class="home-intro">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="well card">
+                <h2>Find a new career</h2>
+                <h3>Here at Sheffield Teaching Hospitals something something bringing innovative stuff with classic other promises</h3>
+                <div class="row">
+                  <div class="col-md-4 col-sm-4 col-xs-6">
+                    <img src="<?php echo get_template_directory_uri() . "/images/andrew_cash_web_small.jpg" ;?>" class="img-responsive">
+                  </div>
+
+                  <div class="col-md-8">
+                    <p class="lead">The eight Clinical Commissioning Groups (CCGs) in South Yorkshire and North Derbyshire, along with NHS England (South Yorkshire and Bassetlaw) and Mid Yorkshire CCGs have formed a similar partnership called 'Working Together'.
+                   </p>
+
+                  <a role="button" href="#" class="btn btn-warning">Further information</a>
+
                   </div>
                 </div>
               </div>
-
-              <div class="col-md-7">
-                <iframe width="100%" height="315" src="https://www.youtube.com/embed/gg-8c9MBrjo?rel=0&amp;controls=0" frameborder="0" allowfullscreen></iframe>
-                <a href="#" class="btn btn-block btn-primary btn-lg">Something something something</a>
-               </div>
             </div>
           </div>
+        </section>
 
           <section class="home-boxes">
             <div class="row">
               <div class="col-md-4 col-sm-4">
                 <div class="home-boxes-unit">
                   <a href="<?php echo get_home_url() . "/current-vacancies/" ;?>">
-                    <img alt="" src="<?php echo get_template_directory_uri() . "/images/nurses-with-computers.jpg" ; ?>" width="100%">
+                    <img alt="" src="<?php echo get_template_directory_uri() . "/images/nurses-with-computers.jpg" ;?>" width="100%">
                     <div class="well">
                       <p class="lead">Current Vacancies</p>
+                      <p class="text">Healthcare Scientist, Nurse or Search our latest vacancies and find your next career move at Sheffield Teaching Hospitals</p>
+                      <div class="btn btn-warning">Find out more</div>
                     </div>
                   </a>
                 </div>
@@ -50,9 +56,11 @@
               <div class="col-md-4 col-sm-4">
                 <div class="home-boxes-unit">
                   <a href="<?php echo get_home_url() . "/nursing-midwifery/" ;?>">
-                    <img alt="" src="<?php echo get_template_directory_uri() . "/images/box_images_nurses.jpg" ; ?>" width="100%">
+                    <img alt="" src="<?php echo get_template_directory_uri() . "/images/box_images_nurses.jpg" ;?>" width="100%">
                     <div class="well">
                       <p class="lead">Nursing at Sheffield</p>
+                      <p class="text">With over nine of our Chief Nurses having started their careers with us, find out what makes Sheffield Teaching Hospitals a great place for Nursing and Midwifery</p>
+                      <div class="btn btn-warning">Find out more</div>
                     </div>
                   </a>
                 </div>
@@ -60,16 +68,42 @@
               
               <div class="col-md-4 col-sm-4">
                 <div class="home-boxes-unit">
-                  <a href="<?php echo get_home_url() . "/current-vacancies/" ;?>">
-                    <img alt="" src="<?php echo get_template_directory_uri() . "/images/hilary.jpg" ; ?>" width="100%">
+                  <a href="#">
+                    <img alt="" src="<?php echo get_template_directory_uri() . "/images/hilary.jpg" ;?>" width="100%">
                     <div class="well">
                       <p class="lead">Meet our Staff</p>
+                      <p class="text">Find out more about your future colleagues and why they love to work in Sheffield and are "Proud" to make a difference in the NHS.</p>
+                      <div class="btn btn-warning">Find out more</div>
                     </div>
                   </a>
                 </div>
               </div>
             </div>
           </section>
+        
+        <section id="our-values">
+           <div class="row">
+            <div class="col-md-12">
+              <div class="well card">
+                <h2>Our Values</h2>
+                <h3>Here at Sheffield Teaching Hospitals something something bringing innovative stuff with classic other promises</h3>
+                <div class="row">
+                  <div class="col-md-4 col-sm-4">
+                    <iframe width="100%" height="250" src="https://www.youtube.com/embed/gg-8c9MBrjo?rel=0&amp;controls=0" frameborder="0" allowfullscreen></iframe>
+                  </div>
+
+                  <div class="col-md-8">
+                    <p class="lead">The eight Clinical Commissioning Groups (CCGs) in South Yorkshire and North Derbyshire, along with NHS England (South Yorkshire and Bassetlaw) and Mid Yorkshire CCGs have formed a similar partnership called 'Working Together'.
+                   </p>
+
+                  <a role="button" href="#" class="btn btn-warning">Further information</a>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
           
           <div class="row">
             

5bde20def880cf2ea0d52620e55797d7307f39ad
GrilloPress <grillopress@gmail.com>
2015-08-21 10:43:08 +0000

    add child page tabs to work for us template

--- wordpress/wp-content/themes/sth/page-work-for-us.php
+++ wordpress/wp-content/themes/sth/page-work-for-us.php
@@ -21,90 +21,87 @@ get_header(); ?>
         <div class="row">
           
           <div id="" class="col-md-8">
-            
             <?php while ( have_posts() ) : the_post(); ?>
+             
+            
+            <div class="tab-content">
+              <div role="tabpanel" class="tab-pane in fade active" id="home">
+                <img src="http://placehold.it/1200x400/cccccc/ffffff" width="100%">
+                <?php get_template_part( 'template-parts/content', 'page' ); ?>
+              </div>
+               <?php endwhile; // End of the loop. ?>
+              
+             
+              <?php 
+                  $args = array(
+                    'post_parent' => 19,
+                    'post_type'   => 'any', 
+                    'numberposts' => -1,
+                    'post_status' => 'any' 
+                  );
 
-              <?php get_template_part( 'template-parts/content', 'page' ); ?>
+               $myposts = get_posts($args);
+               foreach($myposts as $post) :?>
 
-            <?php endwhile; // End of the loop. ?>
+              <?php $post_slug=$post->post_name; ?>
+              <div role="tabpanel" class="tab-pane fade" id="<?php echo $post_slug; ?>">
+              <img src="http://placehold.it/1200x400/cccccc/ffffff" width="100%">
+              <article id="post-<?php the_ID(); ?>" <?php post_class('well'); ?> role="article">
+                  <section class="post_content clearfix">
+                    <h3 class="post-header" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></h3>   
+                    <?php $content = $post->post_content; ?>
+                    <?php echo $content; ?>
+                  </section>
+              </article>
+              </div>
+
+             <?php endforeach; ?>
+
+            </div>
+
+           
           
           </div>
             
             <div class="col-md-4">
               
-              <img src="<?php echo get_template_directory_uri() . "/images/16000-staff.png" ;?>" width="100%">
-                
-              <img src="<?php echo get_template_directory_uri() . "/images/1million.png" ;?>" width="100%">
-                
-              <img src="<?php echo get_template_directory_uri() . "/images/three-trauma.png" ;?>" width="100%">
-              
-              <div class="well well-nav">
-                <ul class="nav nav-pills nav-stacked">
-                  <li role="presentation"><a href="#">Our Vision</a></li>
-                  <li role="presentation"><a href="#">Services &amp; Specialties</a></li>
-                  <li role="presentation"><a href="#">Living in Sheffield</a></li>
-                  <li role="presentation"><a href="#">Staff Benefits</a></li>
+              <div class="nav-controls">
+                 <!-- Nav tabs -->
+                <ul class="nav nav-pills nav-stacked" role="tablist">
+                  
+                  <li role="presentation" class="active">
+                    <a href="#home" aria-controls="home" role="tab" data-toggle="tab">Work for Us</a>
+                  </li>
+                  
+                  <?php 
+                  $args = array(
+                    'post_parent' => 19,
+                    'post_type'   => 'any', 
+                    'numberposts' => -1,
+                    'orderby'   => 'title',
+                    'order' => 'ASC',
+                    'post_status' => 'any' 
+                  );
+
+               $myposts = get_posts($args);
+               foreach($myposts as $post) :?>
+                  
+               <?php $post_slug=$post->post_name; ?>
+                  <li role="presentation">
+                    <a href="#<?php echo $post_slug; ?>" aria-controls="<?php echo $post_slug; ?>" role="tab" data-toggle="tab"><?php the_title(); ?></a>
+                  </li>
+                  
+               <?php endforeach; ?>
+
                 </ul>
               </div>
+              
+              
               
             </div>
 
         </div>
-        
-        <section id="top-benefits" >
-          <div class="row">
-            
-            <div class="col-md-6">
-              <div class="card-benefit">
-                <h3>Specialist Services</h3>
-                <p>As one of the largest NHS Trusts in the Country we provide cutting-edge and specialist services found nowhere else in the country</p>
-                <a href="#">Find out more</a>
-              </div>
-            </div>
-            
-            
-             <div class="col-md-6">
-              <div class="card-benefit">
-                <h3>Multi-site Trust</h3>
-                <p>With two major campuses, the Northern General and Central Campus, you are guarenteed to find variety and a great work location</p>
-                <a href="#">Find out more</a>
-                </div>
-              </div>
-            
-             <div class="col-md-6">
-              <div class="card-benefit">
-                <h3>Training Opportunities</h3>
-                <p>Being a Teaching Hospital we know the value of education and staff development. We offer advanced internal training and chances for top-up degrees</p>
-                <a href="#">Find out more</a>
-              </div>
-            </div>
-            
-             <div class="col-md-6">
-              <div class="card-benefit">
-                <h3>Award Winning Trust</h3>
-                <p>With numerous award winning developments across the Trust, we value and promote innovative thinking and offer great research opportunities</p>
-                <a href="#">Find out more</a>
-              </div>
-            </div>
-            
-             <div class="col-md-6">
-              <div class="card-benefit">
-                <h3>Career Development</h3>
-                <p>With over 90% of our Band 7 Nurses hired from intenral candidates you are assured that we reward skill, passion and excellence</p>
-                <a href="#">Find out more</a>
-              </div>
-            </div>
-            
-             <div class="col-md-6">
-              <div class="card-benefit">
-                <h3>Staff Benefits</h3>
-                <p>Our two dedicated Nurseries, staff events, groups and activites to discounts and vouchers make sure your work experience is highly rewarding </p>
-                <a href="#">Find out more</a>
-              </div>
-            </div>
-          
-          </div>
-        </section>
+
         
         
       </main>

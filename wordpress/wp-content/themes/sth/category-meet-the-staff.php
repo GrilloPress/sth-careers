7f80514c5bb7887f4a605db211f154c37acb482e
GrilloPress <grillopress@gmail.com>
2015-09-03 14:15:11 +0000

    add opacity to images within links with :hover active

--- wordpress/wp-content/themes/sth/category-meet-the-staff.php
+++ wordpress/wp-content/themes/sth/category-meet-the-staff.php
@@ -12,8 +12,10 @@ get_header(); ?>
 
   <div class="row">
     <div class="col-md-12">
-      <h1 class="feature-heading">Meet our Staff</h1>
-      <p class="feature-lead">Find out more about our amazing staff and why they are proud to work at Sheffield Teaching Hospitals</p>
+      <header>
+        <h1 class="feature-heading">Meet our Staff</h1>
+        <p class="feature-lead">Find out more about our amazing staff and why they are proud to work at Sheffield Teaching Hospitals</p>
+      </header>
     </div>
   </div>
   

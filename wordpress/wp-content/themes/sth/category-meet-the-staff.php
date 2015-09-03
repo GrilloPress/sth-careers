f0d15042b871aa16fef8d08330c7f230db3cee4d
GrilloPress <grillopress@gmail.com>
2015-09-03 15:55:24 +0000

    add fade-in styles to meet the staff category page

--- wordpress/wp-content/themes/sth/category-meet-the-staff.php
+++ wordpress/wp-content/themes/sth/category-meet-the-staff.php
@@ -28,7 +28,7 @@ get_header(); ?>
 
 
                       
-                      <section class="col-sm-4 col-md-4">
+                      <section class="col-sm-4 col-md-4 fade-in" style="display: none;">
                         <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
                         <?php if ( has_post_thumbnail()) : ?>
                          <?php the_post_thumbnail('full', array('class' => 'full-width')); ?>
@@ -51,5 +51,17 @@ get_header(); ?>
     </main>
   </div>
 </div>
+
+<script>
+
+  // fade in divs
+  var fadein = jQuery('section.fade-in');
+  jQuery.each(fadein, function(i, item) {
+    setTimeout(function() {
+      jQuery(item).fadeIn(1000); // duration of fadein
+    }, 1000 * i); // duration between fadeins
+  });
+
+</script>
 
 <?php get_footer(); ?>

0beba2fb41f975f9db8efd85b43da160b71b60c6
GrilloPress <grillopress@gmail.com>
2015-09-16 09:53:00 +0000

    update brand header to stay visible in mobile devices

--- wordpress/wp-content/themes/sth/header.php
+++ wordpress/wp-content/themes/sth/header.php
@@ -42,12 +42,12 @@
 <body <?php body_class(); ?>>
   
   <header role="banner">
-      <div id="site-header" class="hidden-xs hidden-sm">
+      <div id="site-header">
         <div class="container">
           <div class="row">
             <div class="col-md-12">
-              <a class="navbar-brand" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
-              <img class="pull-right" src="<?php echo get_template_directory_uri() . "/images/logo.gif"; ?>">
+              <a class="navbar-brand hidden-xs" title="<?php echo get_bloginfo('description'); ?>" href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a>
+              <img class="pull-right img-responsive" src="<?php echo get_template_directory_uri() . "/images/logo.gif"; ?>">
             </div>
           </div>
         </div>

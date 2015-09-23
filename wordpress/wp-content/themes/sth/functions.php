66d215416bfa84c8985e356537bff6ed978bb8c2
GrilloPress <grillopress@gmail.com>
2015-09-23 11:11:43 +0000

    move page-jobs.php script into standalone file

--- wordpress/wp-content/themes/sth/functions.php
+++ wordpress/wp-content/themes/sth/functions.php
@@ -224,6 +224,11 @@ function remove_jp_styles() {
 }
 add_action('wp_print_styles', 'remove_jp_styles');
 
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
 /**
  * Custom template tags for this theme.
  */
--- wordpress/wp-content/themes/sth/js/job.js
+++ wordpress/wp-content/themes/sth/js/job.js
@@ -1,27 +1,127 @@
- jQuery(document).ready(function() {
+ var getUrlParameter = function getUrlParameter(sParam) {
+   var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+       sURLVariables = sPageURL.split('&'),
+       sParameterName,
+       i;
    
-   // This is horrendous. Make a function. Then call it.
-   jQuery('div#results').find(".vacancy.well:contains('Administrative & Clerical')").addClass("administrative-clerical");
-   jQuery('div#results').find(".vacancy.well:contains('Allied Health Professionals')").addClass("allied-health");
-   jQuery('div#results').find(".vacancy.well:contains('Additional Clinical Services')").addClass("additional-clinical");
-   jQuery('div#results').find(".vacancy.well:contains('Additional Professional Scientific & Technical')").addClass("additional-professional");
-   jQuery('div#results').find(".vacancy.well:contains('Estates & Ancillary')").addClass("estates-ancillary");
-   jQuery('div#results').find(".vacancy.well:contains('Healthcare Scientists')").addClass("healthcare-scientists");
-   jQuery('div#results').find(".vacancy.well:contains('Medical & Dental')").addClass("medical-dental");
-   jQuery('div#results').find(".vacancy.well:contains('Nursing & Midwifery')").addClass("nursing-midwifery");
-   jQuery('div#results').find(".vacancy.well:contains('Students')").addClass("students");
-   
-   // add staff group, job type
-   // 
-   // get the inputs by ID. Handle clicks to them. Toggle hide()
-                
-   
-});
+   for (i = 0; i < sURLVariables.length; i++) {
+     sParameterName = sURLVariables[i].split('=');
+     
+     if (sParameterName[0] === sParam) {
+       return sParameterName[1] === undefined ? true : sParameterName[1];
+       }
+     }
+   };
 
-// This is the start of creating event handlers for the input checkboxes. They will also eventually need to fire a recount for the jobs returned.
+jQuery(document).ready(function() {
+  
+  onStart = true;
+  
+  if (onStart){
+    
+    // On page load this counts the available job posts and injects the count in the job count span
+    initalCount = jQuery('.vacancy:visible').length;
+    jQuery( "span.job-count" ).text( initalCount );
+    onStart = false;
+    
+    }
+  
+  // This is horrendous. Make a function. Then call it.
+  jQuery('div#results').find(".vacancy.well:contains('Administrative & Clerical')").addClass("administrative-clerical");
+  jQuery('div#results').find(".vacancy.well:contains('Allied Health Professionals')").addClass("allied-health");
+  jQuery('div#results').find(".vacancy.well:contains('Additional Clinical Services')").addClass("additional-clinical");
+  jQuery('div#results').find(".vacancy.well:contains('Additional Professional Scientific & Technical')").addClass("additional-professional");
+  jQuery('div#results').find(".vacancy.well:contains('Estates & Ancillary')").addClass("estates-ancillary");
+  jQuery('div#results').find(".vacancy.well:contains('Healthcare Scientists')").addClass("healthcare-scientists");
+  jQuery('div#results').find(".vacancy.well:contains('Medical & Dental')").addClass("medical-dental");
+  jQuery('div#results').find(".vacancy.well:contains('Nursing & Midwifery')").addClass("nursing-midwifery");
+  jQuery('div#results').find(".vacancy.well:contains('Students')").addClass("students");
+  jQuery('div#results').find(".vacancy.well:contains('Permanent')").addClass("permanent");
+  jQuery('div#results').find(".vacancy.well:contains('Fixed Term')").addClass("fixed-term");
+  jQuery('div#results').find(".vacancy.well:contains('Apprenticeship')").addClass("apprenticeship");
+  jQuery( "div#results a" ).attr('target', '_blank');
+  
+  });
 
-jQuery( "input#admin-clerical" ).click(function() {
+if (getUrlParameter("referrer")){
   
-  jQuery( ".administrative-clerical" ).toggle();
+  var referralSearchTerm = getUrlParameter("referrer");
+  // Loop through the comment list
+  jQuery("div.vacancy:visible").each(function(){
+    // If the list item does not contain the text phrase fade it out
+    if (jQuery(this).text().search(new RegExp(referralSearchTerm, "i")) < 0) {
+      
+      jQuery(this).hide();
+      // Show the list item if the phrase matches and increase the count by 1
+      } else {
+        jQuery(this).show();
+      }
+    
+    });
   
-});
+  // Scroll to results once filtered
+  jQuery('html, body').animate({
+    
+    scrollTop: jQuery("#results").offset().top - 50
+    });
+  
+  };
+          
+          
+          // jQuery('#master').change(function(){
+          jQuery(document).on('change keyup keypress', '#master', function(e){
+            
+            if ( e.which == 13 ) return false;
+            if ( e.which == 13 ) e.preventDefault();
+            
+            jQuery(".vacancy").show();
+            
+            var staffGroupValue = jQuery("#nhs-staff-groups input[type='radio']:checked").val();
+            if (staffGroupValue){
+              
+              var staffGroupSearch = "." + staffGroupValue;
+              jQuery('div.vacancy:visible').not(staffGroupSearch).hide();
+              
+            }
+            
+            var jobTypeValue = jQuery("#nhs-job-type input[type='radio']:checked").val();
+            if (jobTypeValue){
+              
+              var jobTypeSearch = "." + jobTypeValue;
+              jQuery('div.vacancy:visible').not(jobTypeSearch).hide();
+              
+            }
+            
+            var jobSearchValue = jQuery("#job-search input[type='text']").val();
+            if (jobSearchValue){
+
+              // Loop through the comment list
+              jQuery("div.vacancy:visible").each(function(){
+
+                  // If the list item does not contain the text phrase fade it out
+                  if (jQuery(this).text().search(new RegExp(jobSearchValue, "i")) < 0) {
+                      jQuery(this).hide();
+
+                  // Show the list item if the phrase matches and increase the count by 1
+                  } else {
+                      jQuery(this).show();
+                  }
+              });
+              
+            }
+            
+            // After the results are sorted, it counts them and updated the job count element
+            jobCount = jQuery('.vacancy:visible').length;
+            jQuery( "span.job-count" ).text( jobCount );
+              
+            // After the results are sorted, it finishes by scrolling the browser to the top of the results id
+            if ( jQuery( window ).width() < 992 ){
+              
+              jQuery('html, body').animate({
+                  scrollTop: jQuery("#results").offset().top - 50
+              }, 1000);
+              
+            }
+            
+            
+          });

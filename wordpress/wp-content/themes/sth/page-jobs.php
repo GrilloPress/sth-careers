6d68ff81d7132b512ab0542a3bc294f6578418c3
GrilloPress <grillopress@gmail.com>
2015-10-01 16:17:29 +0000

    add generic job alerts email mailing list to job search template

--- wordpress/wp-content/themes/sth/page-jobs.php
+++ wordpress/wp-content/themes/sth/page-jobs.php
@@ -190,6 +190,52 @@ get_header(); ?>
                     </div>
                   </div>
                   
+                  <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="panel panel-primary">
+                      <div class="panel-heading">
+                        <h5 class="panel-title">
+                          <a class="" role="button" data-toggle="collapse" href="#signUpForm" aria-expanded="true" aria-controls="collapseExample">
+                            Sign up for Alerts <span class="caret"></span>
+                          </a>
+                        </h5>
+                      </div>
+
+                      <div class="panel-body collapse" id="signUpForm" aria-expanded="true">
+
+                        <div id="mc_embed_signup">
+                        <form action="//nhs.us3.list-manage.com/subscribe/post?u=9502add7e8a515cb0c8d0bf4e&amp;id=f9cc5617c3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+                            <div id="mc_embed_signup_scroll">
+
+                        <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                        <div class="mc-field-group">
+                          <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                        </label>
+                          <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                        </div>
+                        <div class="mc-field-group">
+                          <label for="mce-FNAME">First Name </label>
+                          <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                        </div>
+                        <div class="mc-field-group">
+                          <label for="mce-LNAME">Last Name </label>
+                          <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                        </div>
+                          <div id="mce-responses" class="clear">
+                            <div class="response" id="mce-error-response" style="display:none"></div>
+                            <div class="response" id="mce-success-response" style="display:none"></div>
+                          </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;"><input type="text" name="b_9502add7e8a515cb0c8d0bf4e_f9cc5617c3" tabindex="-1" value=""></div>
+                            <div class="clear">
+                              <br>
+                              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-block btn-warning"></div>
+                            </div>
+                        </form>
+                        </div>
+                        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+                      </div>
+                    </div>
+                  </div>
+                  
                   
                   
                 </div>                
